7177c7b632bde4a6b2d3bc035ad2c1fa34d8199a
bt77 <tran.bri@northeastern.edu>
2025-05-04 02:19:07 -0700

    Initial commit

--- IOS101-Prework/ViewController.swift
+++ IOS101-Prework/ViewController.swift
@@ -14,6 +14,43 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+        
+        func changeColor() -> UIColor{
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+            
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+    }
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var school: UILabel!
+    @IBOutlet weak var occupation: UILabel!
+    
+    @IBAction func changeTextColor(_ sender: UIButton) {
+        
+        if name.textColor == .white {
+            name.textColor = .black
+        } else {
+            name.textColor = .white
+        }
+        
+        if school.textColor == .white {
+            school.textColor = .black
+        } else {
+            school.textColor = .white
+        }
+        
+        if occupation.textColor == .white {
+            occupation.textColor = .black
+        } else {
+            occupation.textColor = .white
+        }
+    }
 }
 
